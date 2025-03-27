37682ff4da76e9c2fb9a7b54dffe6d95f7e1cc81
Caitlyn Kim <cakim@smith.edu>
2025-03-27 13:04:04 -0400

    added interfaces

---
+++ House.java
@@ -0,0 +1,55 @@
+/* This is a stub for the House class */
+import java.util.ArrayList;
+
+public class House extends Building implements HouseRequirements{
+  
+  private ArrayList<Student> residents; // The <Student> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+  
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    super(name, address, nFloors); //call superclass constructor with name, address, and nFloors parameters
+    this.residents = new ArrayList<Student>(); //initialize ArrayList residents
+    this.hasDiningRoom = hasDiningRoom; //initalize new parameter hasDiningRoom;
+    System.out.println("You have built a house: 🏠");
+  }
+
+  public boolean hasDiningRoom(){
+    return hasDiningRoom;
+  }
+  
+  public int nResidents(){
+    int nResidents = residents.size(); 
+  }
+
+  public void moveIn(Student s){ //go through list and ensure student isn't in there and if true then add student
+    for (int i = 0; i < residents.size; i++){
+      String resident = residents[i];
+      if (resident.contains(s)) {
+
+      } else {
+        residents.add(s);
+      }
+    }
+
+  //   if (this. > 0) {
+  //     this.remainingSeats -= 1;
+  //     System.out.println("There are " + this.remainingSeats + " remaining seats.");
+  //     return passengersOnboard.add(p);
+  // } else {
+  //     return false;
+  // }
+  // }
+
+  public Student moveOut(Student s) {
+    // return the Student who moved out
+  }
+
+  public boolean isResident(Student s){
+
+  }
+
+  public static void main(String[] args) {
+    new House();
+  }
+
+}
