9fdcf67c804b26c2726519f90080f9dcc73a3d63
Caitlyn Kim <cakim@smith.edu>
2025-04-05 16:18:19 -0400

    added to House class

--- House.java
+++ House.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 public class House extends Building implements HouseRequirements{
-  
   protected ArrayList<Student> residents; // The <Student> tells Java what kind of data we plan to store IN the ArrayList
   protected boolean hasDiningRoom;
   
@@ -15,43 +14,44 @@ public class House extends Building implements HouseRequirements{
   }
 
   public boolean hasDiningRoom(){
-    return hasDiningRoom;
+    return this.hasDiningRoom;
   }
   
   public int nResidents(){
-    int nResidents = residents.size(); 
+    int nResidents = this.residents.size(); 
+    return nResidents;
   }
 
   public void moveIn(Student s){ //go through list and ensure student isn't in there and if true then add student
-    for (int i = 0; i < residents.size(); i++){
-      String resident = residents[i];
-      if (resident.contains(s)) {
-        throw new NoSuchElementException("Student " + s + " is already moved in.");
-      } else {
-        residents.add(s);
-        System.out.println("Student " + s + " is already moved in.");
+    for (int i = 0; i < this.residents.size(); i++){
+      Student resident = this.residents.get(i);
+      if (resident.equals(s)) {
+        throw new NoSuchElementException("Student " + s + " is already in the house.");
       }
+    this.residents.add(s);
+    System.out.println("Student " + s + " has moved into the house.");
     }
-
-  //   if (this. > 0) {
-  //     this.remainingSeats -= 1;
-  //     System.out.println("There are " + this.remainingSeats + " remaining seats.");
-  //     return passengersOnboard.add(p);
-  // } else {
-  //     return false;
-  // }
-  // }
+  }
 
   public Student moveOut(Student s) {
-    // return the Student who moved out
+    for (int i = 0; i < this.residents.size(); i++) {
+        Student resident = this.residents.get(i);
+        if (resident.equals(s)) {
+            Student removedStudent = this.residents.remove(i); 
+            System.out.println("Student " + removedStudent + " has moved out.");
+            return removedStudent;
+        }
+    }
+    throw new NoSuchElementException("Student " + s + " does not live in the house."); //throws exception if not found
   }
 
   public boolean isResident(Student s){
-
+    return this.residents.contains(s);
   }
 
   public static void main(String[] args) {
-    new House();
+    House Gillette = new House("Gillette", "Elm St", 5, true);
+    System.out.println(Gillette);
   }
 
 }
